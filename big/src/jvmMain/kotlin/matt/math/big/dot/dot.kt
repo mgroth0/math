ff613ca4e66bcd24f412909637118a982127d494
mgroth0 <mgroth49@gmail.com>
2022-10-03 01:02:38 -0400

    autocommit

--- big/src/jvmMain/kotlin/matt/math/big/dot/dot.kt
+++ big/src/jvmMain/kotlin/matt/math/big/dot/dot.kt
@@ -1,8 +1,10 @@
 package matt.math.big.dot
 
+import matt.collect.itr.forEachNested
 import matt.math.big.plus
 import matt.math.big.times
 import matt.math.big.toApfloat
+import org.apfloat.Apcomplex
 import org.apfloat.Apfloat
 
 infix fun Array<out Apfloat?>.dotA(other: Array<out Apfloat?>): Apfloat {
@@ -13,6 +15,64 @@ infix fun Array<out Apfloat?>.dotA(other: Array<out Apfloat?>): Apfloat {
 	val second = other[x]
 	if (first != null && second != null) {
 	  val r = first*second
+	  ee += r
+	}
+  }
+  return ee
+}
+
+
+
+infix fun Array<Array<Apfloat?>>.dot(other: Array<Array<Apfloat?>>): Apfloat {
+  require(this.size == this[0].size && this.size == other.size && this[0].size == other[0].size)
+  var ee = 0.0.toApfloat()
+  (0 until this.size).forEachNested { x, y ->
+	val first = this[x][y]
+	val second = other[x][y]
+	if (first != null && second != null) {
+	  ee += first*second
+	}
+  }
+  return ee
+}
+
+infix fun Array<Array<Double?>>.dot(other: Array<Array<Double?>>): Apfloat {
+  require(this.size == this[0].size && this.size == other.size && this[0].size == other[0].size)
+  var ee = 0.0.toApfloat()
+  (0 until this.size).forEachNested { x, y ->
+	val first = this[x][y]
+	val second = other[x][y]
+	if (first != null && second != null) {
+	  ee += first*second
+	}
+  }
+  return ee
+}
+
+
+infix fun Array<Apcomplex>.dot(other: Array<Apcomplex>): Apfloat {
+  require(this.size == other.size)
+  var ee = 0.0.toApfloat()
+  (0 until this.size).forEach { x ->
+	val first = this[x]
+	val second = other[x]
+	if (!first.hasImag && !second.hasImag) {
+	  val r = this[x].multiply(other[x])
+	  ee += r
+	}
+  }
+  return ee
+
+}
+
+infix fun Array<Apfloat>.dot(other: Array<Apfloat>): Apfloat {
+  require(this.size == other.size)
+  var ee = 0.0.toApfloat()
+  (0 until this.size).forEach { x ->
+	val first = this[x]
+	val second = other[x]
+	if (!first.hasImag && !second.hasImag) {
+	  val r = this[x]*other[x]
 	  ee += r
 	}
   }
--- mat/src/jvmMain/kotlin/matt/math/mat/dot/dot.kt
+++ mat/src/jvmMain/kotlin/matt/math/mat/dot/dot.kt
@@ -1,6 +1,11 @@
 package matt.math.mat.dot
 
 import matt.collect.itr.forEachNested
+import matt.math.big.plus
+import matt.math.big.times
+import matt.math.big.toApfloat
+import org.apfloat.Apcomplex
+import org.apfloat.Apfloat
 import org.jetbrains.kotlinx.multik.ndarray.data.D2
 import org.jetbrains.kotlinx.multik.ndarray.data.MultiArray
 import org.jetbrains.kotlinx.multik.ndarray.data.get
@@ -33,31 +38,6 @@ infix fun MultiArray<Double, D2>.dot(other: MultiArray<Double, D2>): Double {
   return ee
 }
 
-infix fun Array<Array<Apfloat?>>.dot(other: Array<Array<Apfloat?>>): Apfloat {
-  require(this.size == this[0].size && this.size == other.size && this[0].size == other[0].size)
-  var ee = 0.0.toApfloat()
-  (0 until this.size).forEachNested { x, y ->
-	val first = this[x][y]
-	val second = other[x][y]
-	if (first != null && second != null) {
-	  ee += first*second
-	}
-  }
-  return ee
-}
-
-infix fun Array<Array<Double?>>.dot(other: Array<Array<Double?>>): Apfloat {
-  require(this.size == this[0].size && this.size == other.size && this[0].size == other[0].size)
-  var ee = 0.0.toApfloat()
-  (0 until this.size).forEachNested { x, y ->
-	val first = this[x][y]
-	val second = other[x][y]
-	if (first != null && second != null) {
-	  ee += first*second
-	}
-  }
-  return ee
-}
 
 infix fun Array<Array<Float?>>.dot(other: Array<Array<Float?>>): Float {
   require(this.size == this[0].size && this.size == other.size && this[0].size == other[0].size)
@@ -73,34 +53,6 @@ infix fun Array<Array<Float?>>.dot(other: Array<Array<Float?>>): Float {
 }
 
 
-infix fun Array<Apcomplex>.dot(other: Array<Apcomplex>): Apfloat {
-  require(this.size == other.size)
-  var ee = 0.0.toApfloat()
-  (0 until this.size).forEach { x ->
-	val first = this[x]
-	val second = other[x]
-	if (!first.hasImag && !second.hasImag) {
-	  val r = this[x].multiply(other[x])
-	  ee += r
-	}
-  }
-  return ee
-
-}
-
-infix fun Array<Apfloat>.dot(other: Array<Apfloat>): Apfloat {
-  require(this.size == other.size)
-  var ee = 0.0.toApfloat()
-  (0 until this.size).forEach { x ->
-	val first = this[x]
-	val second = other[x]
-	if (!first.hasImag && !second.hasImag) {
-	  val r = this[x]*other[x]
-	  ee += r
-	}
-  }
-  return ee
-}
 
 infix fun MultiArray<Apfloat, D2>.dot(other: MultiArray<Apfloat, D2>): Apfloat {
   require(this.shape[0] == this.shape[1] && this.shape[0] == other.shape[0] && this.shape[1] == other.shape[1])
