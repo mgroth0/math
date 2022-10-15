c22e0653403da472d449a17f955c1d849eaaae23
mgroth0 <mgroth49@gmail.com>
2022-10-15 16:12:45 -0400

    autocommit

--- src/commonMain/kotlin/matt/math/mathable/mathable.kt
+++ src/commonMain/kotlin/matt/math/mathable/mathable.kt
@@ -11,4 +11,44 @@ interface Mathable<M: Mathable<M>> {
   operator fun times(n: Number): M
 }
 
+interface DoubleWrapper<M: DoubleWrapper<M>>: Mathable<M>, Comparable<M> {
+  override fun compareTo(other: M): Int {
+	return asDouble.compareTo(other.asDouble)
+  }
+
+  fun fromDouble(d: Double): M
+  val asDouble: Double
+  override fun plus(m: M): M {
+	return fromDouble(asDouble + m.asDouble)
+  }
+
+  override fun minus(m: M): M {
+	return fromDouble(asDouble - m.asDouble)
+  }
+
+  override fun div(n: M): M {
+	return fromDouble(asDouble/n.asDouble)
+  }
+
+  override fun times(n: M): M {
+	return fromDouble(asDouble*n.asDouble)
+  }
+
+  override fun plus(m: Number): M {
+	return fromDouble(asDouble + m.toDouble())
+  }
+
+  override fun minus(m: Number): M {
+	return fromDouble(asDouble - m.toDouble())
+  }
+
+  override fun div(n: Number): M {
+	return fromDouble(asDouble/n.toDouble())
+  }
+
+  override fun times(n: Number): M {
+	return fromDouble(asDouble*n.toDouble())
+  }
+
+}
 
