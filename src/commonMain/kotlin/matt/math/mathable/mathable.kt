2f1a710f57c8abb37746e2a3c90a7781699f2bc1
mgroth0 <mgroth49@gmail.com>
2022-10-15 14:31:00 -0400

    autocommit

--- src/commonMain/kotlin/matt/math/mathable/mathable.kt
+++ src/commonMain/kotlin/matt/math/mathable/mathable.kt
@@ -2,5 +2,9 @@ package matt.math.mathable
 
 interface Mathable<M: Mathable<M>> {
   operator fun plus(m: M): M
+  operator fun minus(m: M): M
   operator fun div(n: Number): M
+  operator fun times(n: Number): M
 }
+
+
